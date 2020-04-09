7d96d933562427b129546bfd121fd0e84b24af89
xandaoUdi <alexandrecostaoliv88@gmail.com>
2020-04-09 16:27:33 -0300

    override routes

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -51,14 +51,25 @@ module.exports = (app) => {
   app.post('/livros', function (req, resp) {
     console.log(req.body)
     const livroDao = new LivroDao(db)
+    
     livroDao.adiciona(req.body)
       .then(resp.redirect('/livros'))
       .catch(erro => console.log(erro))
   })
 
+  app.put('/livros', function(req, resp) {
+    console.log(req.body);
+    const livroDao = new LivroDao(db);
+    
+    livroDao.atualiza(req.body)
+            .then(resp.redirect('/livros'))
+            .catch(erro => console.log(erro));
+});
+
   app.delete('/livros/:id', function (req, resp) {
     const id = req.params.id
     const livroDao = new LivroDao(db)
+    
     livroDao.remove(id)
       .then(() => resp.status(200).end())
       .catch(erro => console.log(erro))
